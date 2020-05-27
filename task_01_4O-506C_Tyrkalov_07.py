455dd4578359860d60c8dcc9c37067be24a05ebf
Никита Т <65958048+tn018@users.noreply.github.com>
2020-05-27 23:49:58 +0300

    Update task_01_4O-506C_Tyrkalov_07.py

--- task_01_4O-506C_Tyrkalov_07.py
+++ task_01_4O-506C_Tyrkalov_07.py
@@ -1,26 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
 
-if not os.path.exists('results'):
-    os.mkdir('results')
-    
+try: os.mkdir('results')
+except OSError: pass
+
+w = open('results/task_01_4O-506C_Tyrkalov_07.txt', 'w')
+print('x{0:4}y(x)\n'.format(' '), file = w)
 A = 512
-
 x = np.linspace(-512,512,1025)
 y = -(A+47) * np.sin(np.sqrt(abs((x/2) + (A+47)))) - x * np.sin(np.sqrt(abs((x - (A+47)))))
+for i in range(0,1025):
+    print('{0}{1:4}{2}'.format(x[i],' ',y[i]), file = w)
+w.close()
 
 plt.plot(x,y)
 plt.xlabel('x')
-plt.ylabel('f(x)')
+plt.ylabel('y(x)')
 plt.grid()
 plt.show()
-
-p = os.path.join(os.getcwd(),'results','task_02_4O-506C_Tyrkalov_07.txt')
-    
-with open(p, 'w') as file:
-    file.write('x{0:4}y(x)\n'.format(' '))
-    for i in range(0,1025):
-        file.write('{0}{1:4}{2}\n'.format(str(x[i]),' ',str(y[i])))
-        
-
