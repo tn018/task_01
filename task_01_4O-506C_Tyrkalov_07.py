dac6fec8b143327337e1f0a67bfec4f8392caed9
Никита Т <65958048+tn018@users.noreply.github.com>
2020-05-28 13:52:29 +0300

    Update task_01_4O-506C_Tyrkalov_07.py

--- task_01_4O-506C_Tyrkalov_07.py
+++ task_01_4O-506C_Tyrkalov_07.py
@@ -1,20 +1,21 @@
 import numpy as np
-import os
 import matplotlib.pyplot as plt
+import os
 
 try: os.mkdir('results')
 except OSError: pass
 
-w = open('results/task_01_4O-506C_Tyrkalov_07.txt', 'w')
-print('x{0:4}y(x)\n'.format(' '), file = w)
 A = 512
 x = np.linspace(-512,512,1025)
-y = -(A+47) * np.sin(np.sqrt(abs((x/2) + (A+47)))) - x * np.sin(np.sqrt(abs((x - (A+47)))))
-for i in range(0,1025):
-    print('{0}{1:4}{2}'.format(x[i],' ',y[i]), file = w)
-w.close()
+y = (-(A+47) * np.sin(np.sqrt(abs((x/2) + (A+47))))
+     - x * np.sin(np.sqrt(abs((x - (A+47))))))
 
-plt.plot(x,y)
+STOLBIKI = np.column_stack((x, y))
+PROBEL = ' '
+np.savetxt('results/task_01_4O-506C_Tyrkalov_07.txt',
+           STOLBIKI, delimiter=f'{4*PROBEL}')
+
+plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('y(x)')
 plt.grid()
